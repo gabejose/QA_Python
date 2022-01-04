9656217498c13e049a4019ed3ca58030d566cc65
gabejose <g.jose10xx@gmail.com>
2022-01-04 17:27:52 +0000

    added info about multiline strings

---
+++ 002-Strings/MultilineStrings.py
@@ -0,0 +1,17 @@
+# Multiline Strings
+# You can assign a multiline string to a variable by using
+# three single or quotes:
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+print(a)
+
+b = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+
+print(b)
