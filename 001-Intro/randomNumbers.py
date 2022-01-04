779e19ce962dd61dbac9ba1346700cee258b93e0
gabejose <g.jose10xx@gmail.com>
2022-01-04 16:34:54 +0000

    added info about random numbers

--- 001-Intro/randomNumbers.py
+++ 001-Intro/randomNumbers.py
@@ -1,3 +1,7 @@
+# Python has a built - in module called random that can be
+# used to make random numbers
+
 import random
 
+# Displays a random number between 1 and 9:
 print(random.randrange(1, 10))
