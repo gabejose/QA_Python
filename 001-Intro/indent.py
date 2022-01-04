8f6d4835129f9ae41423906e09547b1747408f75
gabejose <g.jose10xx@gmail.com>
2022-01-04 10:37:11 +0000

    all about indentation

---
+++ 001-Intro/indent.py
@@ -0,0 +1,12 @@
+# In Python, indentation refers to the spaces at the beginning
+# of a code line
+# Very important in Python compared to other languages
+# Indentation is used to indicate a block of code
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+# There would be a syntax error if you miss the indentation
+# Must be at least one indentation
+# Must als use the same number of spaces in the same block of
+# code
