5d86924c1a8f1ecbeab0f56a1e16d5fa7d8a5a98
gabejose <g.jose10xx@gmail.com>
2022-01-04 17:02:37 +0000

    added casting info

---
+++ 001-Intro/casting.py
@@ -0,0 +1,18 @@
+# Casting
+# This is when you want to specify a type on to a variable
+# This is done using constructor functions:
+
+# - int() constructs an integer number from an integer literal,
+# a float literal (by removing all decimals), or a string literal (
+# assuming the string represents a whole number)
+
+# - float() constructs a float number from an integer literal,
+# a float literal or a string literal (provided the string represents
+# a float or integer)
+
+# - str() constructs a string from a wide variety of data types,
+# including strings, integer literals and float literals
+
+x = int(1)
+y = float(2.8)
+z = int("4")
