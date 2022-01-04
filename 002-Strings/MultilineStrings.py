17487c22db0aa34759f67ef22f661d284dc81113
gabejose <g.jose10xx@gmail.com>
2022-01-04 19:27:13 +0000

    strings are an array of characters

--- 002-Strings/MultilineStrings.py
+++ 002-Strings/MultilineStrings.py
@@ -15,3 +15,12 @@ sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua.'''
 
 print(b)
+
+# Strings in Python are arrays of bytes representing unicode
+# characters
+# However, there is no character data type, so a single character
+# is a string of length 1
+# Square bracekts can be used to access elements of the string
+
+c = "Good Mornng World!"
+print(c[1])
