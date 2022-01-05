2dcbe2a99251f7ca9ecc374fb54dbdacff60b7ce
gabejose <g.jose10xx@gmail.com>
2022-01-05 10:51:44 +0000

    all about escape characters

---
+++ 002-Strings/EscapeCharacter.py
@@ -0,0 +1,20 @@
+# Escape Characters
+
+# To insert characters that are illegal in a string, use
+# an escape character
+# This is a backslash \ followed by the character you want to
+# insert
+
+# An example of an illegal character is a double quote
+# inside a string that is surrounded by double quotes
+
+# E.g.
+# txt = "We don't take kindly to "YOUR" types around here!"
+
+# To fix this:
+
+txt = "We don't take kindly to \"YOUR\" types around here!"
+print(txt)
+
+# Refer to https://www.w3schools.com/python/python_strings_escape.asp
+# for more escape characters in Python
