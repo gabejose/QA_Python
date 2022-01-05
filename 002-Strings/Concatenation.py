b87b8bb5b047293ecae74371639eb851513ae241
gabejose <g.jose10xx@gmail.com>
2022-01-05 09:06:47 +0000

    added info about concatenation

---
+++ 002-Strings/Concatenation.py
@@ -0,0 +1,11 @@
+# String Concatenation
+
+# Use the + operator to combine two strings
+
+a = "Good"
+b = "Morning"
+c = "World!"
+
+# Add a space between the quotes to add a space between strings
+d = a + " " + b + " " + c
+print(d)
