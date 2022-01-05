93efaa117d2b7d5ce8ab10c2aea86e4a4458457a
gabejose <g.jose10xx@gmail.com>
2022-01-05 14:28:46 +0000

    added arithmetic operators

---
+++ 004-Operators/Arithmetic.py
@@ -0,0 +1,23 @@
+x = 10
+y = 4
+
+# Addition
+print(x + y)
+
+# Subtraction
+print(x - y)
+
+# Multiplication
+print(x * y)
+
+# Division
+print(x / y)
+
+# Modulus
+print(x % y)
+
+# Exponentiation
+print(x ** y)
+
+# Floor Division - rounds the result down to nearest whole number
+print(x // y)
