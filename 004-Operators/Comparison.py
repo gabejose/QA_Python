70fc283d06b0da046db848b2735ff4f508b4e3b2
gabejose <g.jose10xx@gmail.com>
2022-01-05 14:40:39 +0000

    added info on comparison operators

---
+++ 004-Operators/Comparison.py
@@ -0,0 +1,9 @@
+# Comparison Operators
+# Used to compare two values
+
+# == - equal
+# != - not equal
+# > - greater than
+# < - less than
+# <= - less than or equal to
+# >= - greater than or equal to
