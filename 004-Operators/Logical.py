03a10a3b65b78b9abe3449bed3a8c7e652224a8e
gabejose <g.jose10xx@gmail.com>
2022-01-05 15:40:00 +0000

    info about logical operators

---
+++ 004-Operators/Logical.py
@@ -0,0 +1,15 @@
+import random
+# Logical Operators
+# Used to combine conditional statements
+
+# and - Returns true if both statements are true
+x = random.randrange(1, 20)
+if x < 3 or x < 10:
+    print(x)
+    print("True!")
+else:
+    print("False!")
+
+# or - Returns true if one of the statements is true
+
+# not - Reverses the result, returns false if the result is true
