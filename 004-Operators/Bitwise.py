9f5bfae03300d965924465d3ba61bf5cd1724e71
gabejose <g.jose10xx@gmail.com>
2022-01-06 12:03:21 +0000

    added info about bitwise operators

---
+++ 004-Operators/Bitwise.py
@@ -0,0 +1,9 @@
+# Bitwise operators
+# Used to compare (binary) numbers
+
+# & (AND) - sets each bit to 1 if both bits are 1
+# | (OR) - sets each bit to 1 if one of two bits is 1
+# ^ (XOR) - sets each bit to 1 if only one of two bits is 1
+# ~ (NOT) - inverts all the bits
+# << (Zero fill left shift) - Shift left by ushing zeros in from the right and let the leftmost bits fall off
+# >> (Signed right shift) - Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
