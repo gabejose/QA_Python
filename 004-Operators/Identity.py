9ff117061e360339e7fdae47225b87a1333a552b
gabejose <g.jose10xx@gmail.com>
2022-01-06 10:19:25 +0000

    added info about is not operator

--- 004-Operators/Identity.py
+++ 004-Operators/Identity.py
@@ -18,3 +18,16 @@ print(x is y)
 # == and 'is' aren't the same (see below)
 # Returns true because x is equal to y
 print(x == y)
+
+# is not - Returns true if both variables aren't the same object
+
+# Returns false as z is the same object as x
+print(x is not z)
+
+# Returns true because x is not the same object as y
+# despite having the same content
+print(x is not y)
+
+# != and 'is not' aren't the same
+# Returns false because x is equal to y
+print(x != y)
