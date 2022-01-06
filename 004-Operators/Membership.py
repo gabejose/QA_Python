81d81f2faf87f93b8f55740bfc223848eadf5633
gabejose <g.jose10xx@gmail.com>
2022-01-06 11:54:41 +0000

    added info about membership operators

---
+++ 004-Operators/Membership.py
@@ -0,0 +1,16 @@
+# Membership Operators
+
+# in - Returns true if a sequence with the specified value
+# is present in the object
+
+x = ["apple", "banana"]
+
+# Returns true because a sequence with the value "banana"
+# is in the list
+print("banana" in x)
+
+# not in - Returns true if a sequence with the specified value
+# is not present in the object
+
+# Returns true because "pineapple" isn't in the list
+print("pineapple" not in x)
