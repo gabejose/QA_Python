95a5960456e3b45f662442bb47201d5f7b30ca0b
gabejose <g.jose10xx@gmail.com>
2022-01-11 12:54:02 +0000

    how to join tuples

---
+++ 005-Collections/Tuples/Join.py
@@ -0,0 +1,17 @@
+# Joining Tuples
+
+# To join two or more tuples, use the + operator
+
+# E.g. Join two tuples
+tuple1 = ("a", "b", "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# Multiply Tuples
+# To multiple the content of a tuple n number of times, use the * operator
+
+fruits = ("apple", "banana", "cherry")
+myTuple = fruits * 2
+print(myTuple)
