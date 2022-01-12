e48ba1d8805f71ad5e98ed0f469eb869b71e7d64
gabejose <g.jose10xx@gmail.com>
2022-01-12 14:08:36 +0000

    use a for loop for sets

---
+++ 005-Collections/Sets/Loop.py
@@ -0,0 +1,9 @@
+# Looping through Sets
+
+# You can use a for loop to iterate  through the set items
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+    print(x)
+
+# Can't use a while loop as items can't be referred to by their index
