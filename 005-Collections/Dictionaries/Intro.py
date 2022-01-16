2327347488a9941092677dcf2e9afebbdd8cbc30
gabejose <g.jose10xx@gmail.com>
2022-01-16 19:12:36 +0000

    how to create a dictionary

---
+++ 005-Collections/Dictionaries/Intro.py
@@ -0,0 +1,18 @@
+# Dictionary
+# Used to store data in key:value pairs
+
+# A dictionary is a collection which is:
+# - ordered (3.6 and earlier, dictionaries weren't ordered)
+# - changeable
+# - no duplicates
+
+# Dictionaries are written with curly brackets, and have keys and values:
+# E.g. Create and print a dictionary:
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+print(thisdict)
