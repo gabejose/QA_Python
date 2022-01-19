d6633b51a0fdc86fe2b6e7254c9703da126d12a9
gabejose <g.jose10xx@gmail.com>
2022-01-19 10:24:41 +0000

    bad practice

---
+++ 009-Classes and Objects/Intro.py
@@ -0,0 +1,10 @@
+# Classes and Objects in Python
+
+# To create a class, use the keyword class:
+class MyClass:
+    x = 5
+
+
+# Now we can use the class named MyClass to create objects:
+p1 = MyClass()
+print(p1.x)
