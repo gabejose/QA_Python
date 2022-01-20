1dbd3f231baf5cd9604813269531010c5153f8ed
gabejose <g.jose10xx@gmail.com>
2022-01-20 18:25:51 +0000

    how to stop iteration

--- 011-Iterators/Intro.py
+++ 011-Iterators/Intro.py
@@ -34,3 +34,35 @@ for x in mystr:
 
 # The for loop actually creates an iterator object and executes the next() method
 # for each loop
+
+# Create an Iterator
+# To create an object/class as an iterator you have to implement the methods __iter__()
+# and __next__() to your object
+
+
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    # E.g.Stop after 20 iterations:
+    def __next__(self):
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            print("The iteration has stopped!")
+            raise StopIteration
+
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+    print(x)
+
+# StopIteration
+# This statement is used to prevent an iteration from continuing forever
+# In the __next__() method, we can add a terminating condition to raise an error if the iteration
+# is done a specified number of times
